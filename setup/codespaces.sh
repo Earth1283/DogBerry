d393f2a493315521c2cd4d2cd2aeac89d25d2da2
Earth1283 <142399154+Earth1283@users.noreply.github.com>
2026-04-01 15:51:09 +0000

    update codespace setup

--- setup/codespaces.sh
+++ setup/codespaces.sh
@@ -1,2 +1,12 @@
-wget https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.tar.gz
-tar -xzvf jdk-21_linux-x64_bin.tar.gz 
+cd ~
+
+wget https://oracle.com
+
+tar -xzvf jdk-21_linux-x64_bin.tar.gz
+
+JDK_DIR=$(ls -d jdk-21*)
+
+echo "export JAVA_HOME=\"\$HOME/$JDK_DIR\"" >> ~/.bashrc
+echo "export PATH=\"\$JAVA_HOME/bin:\$PATH\"" >> ~/.bashrc
+
+source ~/.bashrc
